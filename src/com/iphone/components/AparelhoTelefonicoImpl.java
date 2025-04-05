fd5d809d98bebc529ee51a640c51153d31a1df99
rodrigobarr0s <rodrigobarros.dev@gmail.com>
2025-04-05 11:33:49 -0300

    Adicionada classe AparelhoTelefonicoImpl no pacote com.iphone.components

---
+++ src/com/iphone/components/AparelhoTelefonicoImpl.java
@@ -0,0 +1,18 @@
+package com.iphone.components;
+
+public class AparelhoTelefonicoImpl implements AparelhoTelefonico {
+    @Override
+    public void ligar(String numero) {
+        System.out.println("Ligando para o número: " + numero);
+    }
+
+    @Override
+    public void atender() {
+        System.out.println("Atendendo a chamada...");
+    }
+
+    @Override
+    public void iniciarCorreioVoz() {
+        System.out.println("Iniciando correio de voz...");
+    }
+}
