001325bbf18c9d8026a9d3b984624245b22ff1db
rodrigobarr0s <rodrigobarros.dev@gmail.com>
2025-04-05 10:59:13 -0300

    Adicionada classe ReprodutorMusicalImpl no pacote com.iphone.components

---
+++ src/com/iphone/components/ReprodutorMusicalImpl.java
@@ -0,0 +1,19 @@
+package com.iphone.components;
+
+public class ReprodutorMusicalImpl implements ReprodutorMusical {
+	
+    @Override
+    public void tocar() {
+        System.out.println("Tocando música...");
+    }
+
+    @Override
+    public void pausar() {
+        System.out.println("Música pausada.");
+    }
+
+    @Override
+    public void selecionarMusica(String musica) {
+        System.out.println("Selecionando música: " + musica);
+    }
+}
